63d15fbeee2c73c1951ed5c4fafabc0cf65e6f0b
Asiya Akter <asiyaakterpriya24@gmail.com>
2023-07-14 12:27:23 +0600

    new add

---
+++ topnavigationbar.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Top Navigation Bar</title>
+</head>
+<body>
+    <table>
+        <tr>
+            <td>
+                <table>
+                    <tr>
+                        <td align="left">
+                            <img src="logo.jpg" alt="Logo" height="20px" width="30px">
+                        </td>
+                    </tr>
+                </table>
+            </td>
+            <td>
+                <table>
+                    <tr>
+                        <td align="center">
+                            <form>
+                                <input type="text" placeholder="Search...">
+                                <button type="submit">
+                                    <img src="search.png" alt="Search" height="20px" width="20px">
+                                </button>
+                            </form>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+            <td >
+            <table class="right-align">
+    <tr>
+      <td>
+        <a href="#">
+          <img src="profile.png" alt="Profile" height="20px" width="30px">
+        </a>
+        <a href="sign.php">
+          <img src="logout.png" alt="Logout" height="20px" width="30px">
+        </a>
+      </td>
+    </tr>
+  </table>
+            </td>
+        </tr>
+    </table>
+
+    <table align="center">
+        <tr>
+            <td>
+                <a href="#">About Us</a>
+            </td>
+            <td>
+                <a href="#">FAQ</a>
+            </td>
+            <td>
+                <a href="#">Support</a>
+            </td>
+        </tr>
+    </table>
+
+    <footer>
+        <p>&copy; 2023 Your Website. All rights reserved. | Designed by Your Name</p>
+        <div>
+            <a href="https://www.facebook.com/yourpage" target="_blank">Facebook</a> |
+            <a href="mailto:info@gmail.com">Email</a>
+        </div>
+    </footer>
+</body>
+</html>
+
+                          
