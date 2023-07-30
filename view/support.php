cdb73b6782d711971666f50226c7437a70ce5f09
Asiya Akter <asiyaakterpriya24@gmail.com>
2023-07-30 22:16:38 +0600

    Update support.php

--- view/support.php
+++ view/support.php
@@ -10,8 +10,101 @@
 
 <head>
     <title>Professional sage | Support</title>
-    <link rel="stylesheet" type="text/css" href="support.css">
     <script src="../controller/js/support.js"></script>
+    <style>/* style.css */
+
+/* General Styles */
+
+body {
+    font-family: Arial, sans-serif;
+    line-height: 1.6;
+    margin: 0;
+    padding: 0;
+    background-color: #f9f9f9;
+    color: #333;
+}
+
+.container {
+    max-width: 800px;
+    margin: 0 auto;
+    padding: 20px;
+}
+
+h2 {
+    color: black;
+    text-align: center;
+    font-weight: bold;
+}
+
+b {
+    color: red;
+    font-weight: bold;
+}
+
+i {
+    font-style: italic;
+    
+}
+
+a {
+    color: #007bff;
+    text-decoration: none;
+    font-weight: bold;
+}
+
+a:hover {
+    text-decoration: underline;
+}
+
+
+form {
+    background-color: #fff;
+    padding: 20px;
+    border-radius: 6px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+form label {
+    display: block;
+    margin-bottom: 10px;
+}
+
+form input[type="text"],
+form textarea {
+    width: 100%;
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    margin-bottom: 12px;
+    box-sizing: border-box;
+}
+
+form textarea {
+    resize: vertical;
+}
+
+form button[type="submit"] {
+    background-color: #007bff;
+    color: #fff;
+    border: none;
+    border-radius: 4px;
+    padding: 10px 20px;
+   
+}
+
+form button[type="submit"]:hover {
+    background-color: #0056b3;
+}
+
+
+.msg {
+    color: red;
+    font-size: 16px;
+   
+}
+
+
+</style>
 </head>
 
 <body>
@@ -20,11 +113,11 @@
         <table align="center">
             <tr>
                 <td align="center">
-                    <h3>Support</h3>
+                    <h2>Support</h2>
                     <p align="center"><b><i>*Our admin will update you on the given email. It can take 2-3 days long.</i></b></p>
                     <table align="center">
                         <tr>
-                            <td>Email:</td>
+                            <td >Email:</td>
                             <td>
                                 <input type="text" name="email" id="email" onkeyup="validEmail()">
                                 <div class="msg">
