ce60bf9f50547cb57cc23f92ea437ba2c9bae35f
Asiya Akter <asiyaakterpriya24@gmail.com>
2023-07-30 23:03:04 +0600

    Update add_admin.php

--- view/add_admin.php
+++ view/add_admin.php
@@ -82,108 +82,120 @@ if (isset($_GET['success'])) {
     <script src="../controller/js/add-admin.js"></script>
 
     <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f9f9f9;
-            margin: 0;
-            padding: 0;
+      
+
+body {
+    font-family: Arial, sans-serif;
+    line-height: 1.6;
+    margin: 0;
+    padding: 0;
+    background-color: #f9f9f9;
+    color: #333;
+}
+
+h2 {
+    color: #007bff;
+}
+
+/* Form Styles */
+
+fieldset {
+    background-color: #fff;
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 6px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+form table {
+    width: 100%;
+}
+
+form td {
+    padding: 10px;
+}
+
+form input[type="text"],
+form input[type="email"],
+form input[type="password"],
+form textarea,
+form select {
+    width: 100%;
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+    margin-bottom: 12px;
+}
+
+form textarea {
+    resize: vertical;
+}
+
+form select {
+    appearance: none;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    padding-right: 20px;
+    background: url("down_arrow.png") no-repeat right center;
+}
+
+form button[type="submit"] {
+    background-color: #4CAF50;
+    color: white;
+    border: none;
+    border-radius: 4px;
+    padding: 10px 20px;
+   
+}
+
+form button[type="submit"]:hover {
+    background-color: #45a049;
+}
+#empty_err {
+            color: red;
         }
 
-        /* Top navigation bar styles (assuming you have a top_navbar.php file) */
-        /* Modify these styles according to your design */
-
-        .top-navbar {
-            background-color: #333;
-            color: #fff;
-            padding: 10px;
-        }
-
-        .top-navbar a {
-            color: #fff;
-            text-decoration: none;
-            padding: 8px 16px;
-        }
-
-        .top-navbar a:hover {
-            background-color: #555;
-        }
-
-        /* Centering elements */
-
-        .center {
+        .error-container {
+            color: red;
             text-align: center;
         }
 
-        /* Form field styles */
 
-        fieldset {
-            margin: 20px auto;
-            width: 80%;
-            border: 1px solid #ddd;
-            padding: 20px;
-            background-color: #fff;
-            box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
-        }
+.msg {
+    color: red;
+    font-size: 0.9rem;
+}
 
-        legend {
-            font-weight: bold;
-            font-size: 18px;
-        }
 
-        label {
-            display: block;
-            margin-bottom: 8px;
-        }
+i {
+    font-style: italic;
+    font-size: 0.9rem;
+    color: red;
+}
 
-        input[type="text"],
-        input[type="email"],
-        input[type="tel"],
-        input[type="password"],
-        textarea,
-        select {
-            width: 100%;
-            padding: 10px;
-            border: 1px solid #ddd;
-            border-radius: 5px;
-            margin-bottom: 15px;
-            font-size: 14px;
-        }
+a {
+    color: #1E90FF;
+   
+}
 
-        select {
-            height: 35px;
-        }
+a:hover {
+    text-decoration: underline;
+    color:red;
+}
 
-        textarea {
-            resize: vertical;
-        }
+/* Responsive Styles */
 
-        input[type="submit"],
-        input[type="button"] {
-            background-color: #4CAF50;
-            color: #fff;
-            border: none;
-            padding: 12px 20px;
-            border-radius: 5px;
-            cursor: pointer;
-            font-size: 16px;
-        }
+@media (max-width: 600px) {
+    form input[type="text"],
+    form input[type="email"],
+    form input[type="password"],
+    form textarea,
+    form select {
+        width: 100%;
+    }
+}
 
-        input[type="submit"]:hover,
-        input[type="button"]:hover {
-            background-color: #45a049;
-        }
-
-        /* Error and success message styles */
-
-        .error-msg {
-            color: red;
-            font-size: 14px;
-        }
-
-        .success-msg {
-            color: green;
-            font-size: 14px;
-        }
     </style>
 
 </head>
@@ -217,12 +229,12 @@ if (isset($_GET['success'])) {
         <tr>
             <td>
                 <fieldset>
-                    <legend>Add Admin</legend>
+                    <h2 align="center">Add Admin</h2>
                     <form method="POST" action="../controller/add_admin_process.php" onsubmit="return validate()">
                         <table cellspacing="0" cellpadding="5">
 
                             <tr>
-                                <td bgcolor="#AED6F1  "><strong>Username:</strong></td>
+                                <td ><strong>Username:</strong></td>
                                 <td>
                                     <input type="text" id="username" name="username" onkeyup="checkUsernameExist()">
                                     <br>
@@ -232,15 +244,15 @@ if (isset($_GET['success'])) {
                                 </td>
                             </tr>
                             <tr>
-                                <td bgcolor="#AED6F1 "><strong>First Name:</strong></td>
+                                <td><strong>First Name:</strong></td>
                                 <td><input type="text" id="first-name" name="first_name"></td>
                             </tr>
                             <tr>
-                                <td bgcolor="#AED6F1 "><strong>Last Name:</strong></td>
+                                <td><strong>Last Name:</strong></td>
                                 <td><input type="text" id="last-name" name="last_name"></td>
                             </tr>
                             <tr>
-                                <td bgcolor="#AED6F1 "><strong>Email Address:</strong></td>
+                                <td><strong>Email Address:</strong></td>
                                 <td>
                                     <input type="email" id="email" name="email" onkeyup="checkEmailExist()">
                                     <br>
@@ -250,7 +262,7 @@ if (isset($_GET['success'])) {
                                 </td>
                             </tr>
                             <tr>
-                                <td bgcolor="#AED6F1 "><strong>Gender:</strong></td>
+                                <td ><strong>Gender:</strong></td>
                                 <td>
                                     <select id="gender" name="gender">
                                         <option value="male">Male</option>
@@ -260,7 +272,7 @@ if (isset($_GET['success'])) {
                                 </td>
                             </tr>
                             <tr>
-                                <td bgcolor="#AED6F1 "><strong>Country/Region:</strong></td>
+                                <td><strong>Country/Region:</strong></td>
                                 <td>
                                     <select id="country" name="country">
                                         <option value="USA">United States of America</option>
@@ -273,7 +285,7 @@ if (isset($_GET['success'])) {
                             </tr>
                             <tr>
 
-                                <td bgcolor="#AED6F1 "><strong>Phone Number:</strong></td>
+                                <td ><strong>Phone Number:</strong></td>
                                 <td>
                                     <i>
                                         <font color="red" size="2">*without country code</font>
@@ -282,11 +294,11 @@ if (isset($_GET['success'])) {
                                 </td>
                             </tr>
                             <tr>
-                                <td bgcolor="#AED6F1 "><strong>Address:</strong></td>
+                                <td ><strong>Address:</strong></td>
                                 <td><textarea name="address" id="address" cols="20" rows="3"></textarea></td>
                             </tr>
                             <tr>
-                                <td bgcolor="#AED6F1 "><strong>Password:</strong></td>
+                                <td><strong>Password:</strong></td>
                                 <td><input type="password" id="password" name="password"></td>
                             </tr>
                             <tr>
